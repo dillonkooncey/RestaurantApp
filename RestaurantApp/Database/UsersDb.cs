ded11920e75bd182d83450e3049f7ca1ef9ee0ee
dillonkooncey <dillonkoonce@furniturelandsouth.com>
2026-06-24 22:34:53 -0400

    Started infrastructure for user login and modifying Result class to be more dynamic to handle multiple types returns

---
+++ RestaurantApp/Database/UsersDb.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.SqlClient;
+using RestaurantApp.Database;
+using RestaurantApp.Results;
+using System;
+
+namespace RestaurantApp.Database
+{
+    public class UsersDb
+    {
+        public Result LogIn(string username, string password)
+        {
+            using SqlConnection conn = DbConnectionFactory.CreateConnection();
+
+            try
+            {
+                conn.Open();
+
+                const string accessString = "SELECT 1 FROM Users WHERE Username = @username and Password = @password";
+                using SqlCommand command = new(accessString, conn);
+
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", password);
+
+                using SqlDataReader reader = command.ExecuteReader();
+
+                // Check to see if we could find a user with the entered information
+                if (reader.Read())
+                {
+                    return new Result(true);
+                }
+                else
+                {
+                    return new Result(false, "Username and Password not found. Try again...");
+                }
+            }
+            catch (Exception ex)
+            {
+                return new Result(false, $"Connection to DB Failed: {ex.Message}");
+            }
+        }
+    }
+}
